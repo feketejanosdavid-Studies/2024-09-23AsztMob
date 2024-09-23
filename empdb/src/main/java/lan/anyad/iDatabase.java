6c3361caebe125619a51072ad72bd35e4d0ec602
Fekete János Dávid <janosdavidfekete@gmail.com>
2024-09-23 12:29:49 +0200

    Mostmár működik az empdb

--- empdb/src/main/java/lan/anyad/iDatabase.java
+++
@@ -1,9 +0,0 @@
-package lan.anyad;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface iDatabase {
-    public Connection connectDB() throws SQLException;
-    public void closeDB(Connection con);
-}
